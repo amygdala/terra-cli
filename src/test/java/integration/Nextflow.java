d553cfeba50b88609dc8653d7b3c2a6f8b571337
melissachang <10929390+melissachang@users.noreply.github.com>
2022-02-17 13:53:36 -0800

    50 times each

--- src/test/java/integration/Nextflow.java
+++ src/test/java/integration/Nextflow.java
@@ -19,6 +19,304 @@ public class Nextflow extends ClearContextIntegration {
   @Test
   @DisplayName("nextflow run hello")
   void helloWorld() throws IOException {
+    helloWorld_inner();
+  }
+
+  @Test
+  @DisplayName("nextflow run hello")
+  void helloWorld2() throws IOException {
+    helloWorld_inner();
+  }
+
+  @Test
+  @DisplayName("nextflow run hello")
+  void helloWorld3() throws IOException {
+    helloWorld_inner();
+  }
+
+  @Test
+  @DisplayName("nextflow run hello")
+  void helloWorld4() throws IOException {
+    helloWorld_inner();
+  }
+
+  @Test
+  @DisplayName("nextflow run hello")
+  void helloWorld5() throws IOException {
+    helloWorld_inner();
+  }
+
+  @Test
+  @DisplayName("nextflow run hello")
+  void helloWorld6() throws IOException {
+    helloWorld_inner();
+  }
+
+  @Test
+  @DisplayName("nextflow run hello")
+  void helloWorld7() throws IOException {
+    helloWorld_inner();
+  }
+
+  @Test
+  @DisplayName("nextflow run hello")
+  void helloWorld8() throws IOException {
+    helloWorld_inner();
+  }
+
+  @Test
+  @DisplayName("nextflow run hello")
+  void helloWorld9() throws IOException {
+    helloWorld_inner();
+  }
+
+  @Test
+  @DisplayName("nextflow run hello")
+  void helloWorld10() throws IOException {
+    helloWorld_inner();
+  }
+
+  @Test
+  @DisplayName("nextflow run hello")
+  void helloWorld11() throws IOException {
+    helloWorld_inner();
+  }
+
+  @Test
+  @DisplayName("nextflow run hello")
+  void helloWorld12() throws IOException {
+    helloWorld_inner();
+  }
+
+  @Test
+  @DisplayName("nextflow run hello")
+  void helloWorld13() throws IOException {
+    helloWorld_inner();
+  }
+
+  @Test
+  @DisplayName("nextflow run hello")
+  void helloWorld14() throws IOException {
+    helloWorld_inner();
+  }
+
+  @Test
+  @DisplayName("nextflow run hello")
+  void helloWorld15() throws IOException {
+    helloWorld_inner();
+  }
+
+  @Test
+  @DisplayName("nextflow run hello")
+  void helloWorld16() throws IOException {
+    helloWorld_inner();
+  }
+
+  @Test
+  @DisplayName("nextflow run hello")
+  void helloWorld17() throws IOException {
+    helloWorld_inner();
+  }
+
+  @Test
+  @DisplayName("nextflow run hello")
+  void helloWorld18() throws IOException {
+    helloWorld_inner();
+  }
+
+  @Test
+  @DisplayName("nextflow run hello")
+  void helloWorld19() throws IOException {
+    helloWorld_inner();
+  }
+
+  @Test
+  @DisplayName("nextflow run hello")
+  void helloWorld20() throws IOException {
+    helloWorld_inner();
+  }
+
+  @Test
+  @DisplayName("nextflow run hello")
+  void helloWorld21() throws IOException {
+    helloWorld_inner();
+  }
+
+  @Test
+  @DisplayName("nextflow run hello")
+  void helloWorld22() throws IOException {
+    helloWorld_inner();
+  }
+
+  @Test
+  @DisplayName("nextflow run hello")
+  void helloWorld23() throws IOException {
+    helloWorld_inner();
+  }
+
+  @Test
+  @DisplayName("nextflow run hello")
+  void helloWorld24() throws IOException {
+    helloWorld_inner();
+  }
+
+  @Test
+  @DisplayName("nextflow run hello")
+  void helloWorld25() throws IOException {
+    helloWorld_inner();
+  }
+
+  @Test
+  @DisplayName("nextflow run hello")
+  void helloWorld26() throws IOException {
+    helloWorld_inner();
+  }
+
+  @Test
+  @DisplayName("nextflow run hello")
+  void helloWorld27() throws IOException {
+    helloWorld_inner();
+  }
+
+  @Test
+  @DisplayName("nextflow run hello")
+  void helloWorld28() throws IOException {
+    helloWorld_inner();
+  }
+
+  @Test
+  @DisplayName("nextflow run hello")
+  void helloWorld29() throws IOException {
+    helloWorld_inner();
+  }
+
+  @Test
+  @DisplayName("nextflow run hello")
+  void helloWorld30() throws IOException {
+    helloWorld_inner();
+  }
+
+  @Test
+  @DisplayName("nextflow run hello")
+  void helloWorld31() throws IOException {
+    helloWorld_inner();
+  }
+
+  @Test
+  @DisplayName("nextflow run hello")
+  void helloWorld32() throws IOException {
+    helloWorld_inner();
+  }
+
+  @Test
+  @DisplayName("nextflow run hello")
+  void helloWorld33() throws IOException {
+    helloWorld_inner();
+  }
+
+  @Test
+  @DisplayName("nextflow run hello")
+  void helloWorld34() throws IOException {
+    helloWorld_inner();
+  }
+
+  @Test
+  @DisplayName("nextflow run hello")
+  void helloWorld35() throws IOException {
+    helloWorld_inner();
+  }
+
+  @Test
+  @DisplayName("nextflow run hello")
+  void helloWorld36() throws IOException {
+    helloWorld_inner();
+  }
+
+  @Test
+  @DisplayName("nextflow run hello")
+  void helloWorld37() throws IOException {
+    helloWorld_inner();
+  }
+
+  @Test
+  @DisplayName("nextflow run hello")
+  void helloWorld38() throws IOException {
+    helloWorld_inner();
+  }
+
+  @Test
+  @DisplayName("nextflow run hello")
+  void helloWorld39() throws IOException {
+    helloWorld_inner();
+  }
+
+  @Test
+  @DisplayName("nextflow run hello")
+  void helloWorld40() throws IOException {
+    helloWorld_inner();
+  }
+
+  @Test
+  @DisplayName("nextflow run hello")
+  void helloWorld41() throws IOException {
+    helloWorld_inner();
+  }
+
+  @Test
+  @DisplayName("nextflow run hello")
+  void helloWorld142() throws IOException {
+    helloWorld_inner();
+  }
+
+  @Test
+  @DisplayName("nextflow run hello")
+  void helloWorld43() throws IOException {
+    helloWorld_inner();
+  }
+
+  @Test
+  @DisplayName("nextflow run hello")
+  void helloWorld44() throws IOException {
+    helloWorld_inner();
+  }
+
+  @Test
+  @DisplayName("nextflow run hello")
+  void helloWorld45() throws IOException {
+    helloWorld_inner();
+  }
+
+  @Test
+  @DisplayName("nextflow run hello")
+  void helloWorld46() throws IOException {
+    helloWorld_inner();
+  }
+
+  @Test
+  @DisplayName("nextflow run hello")
+  void helloWorld47() throws IOException {
+    helloWorld_inner();
+  }
+
+  @Test
+  @DisplayName("nextflow run hello")
+  void helloWorld48() throws IOException {
+    helloWorld_inner();
+  }
+
+  @Test
+  @DisplayName("nextflow run hello")
+  void helloWorld49() throws IOException {
+    helloWorld_inner();
+  }
+
+  @Test
+  @DisplayName("nextflow run hello")
+  void helloWorld50() throws IOException {
+    helloWorld_inner();
+  }
+
+  void helloWorld_inner() throws IOException {
     // select a test user and login
     TestUser testUser = TestUser.chooseTestUserWithSpendAccess();
     testUser.login();
@@ -101,6 +399,246 @@ public class Nextflow extends ClearContextIntegration {
   @Test
   @DisplayName("nextflow config and run with GLS tutorial")
   void nextflowFromGLSTutorial10() throws IOException {
+    nextflowFromGLSTutorial_inner();
+  }
+
+  @Test
+  @DisplayName("nextflow config and run with GLS tutorial")
+  void nextflowFromGLSTutorial11() throws IOException {
+    nextflowFromGLSTutorial_inner();
+  }
+
+  @Test
+  @DisplayName("nextflow config and run with GLS tutorial")
+  void nextflowFromGLSTutorial12() throws IOException {
+    nextflowFromGLSTutorial_inner();
+  }
+
+  @Test
+  @DisplayName("nextflow config and run with GLS tutorial")
+  void nextflowFromGLSTutorial13() throws IOException {
+    nextflowFromGLSTutorial_inner();
+  }
+
+  @Test
+  @DisplayName("nextflow config and run with GLS tutorial")
+  void nextflowFromGLSTutorial14() throws IOException {
+    nextflowFromGLSTutorial_inner();
+  }
+
+  @Test
+  @DisplayName("nextflow config and run with GLS tutorial")
+  void nextflowFromGLSTutorial15() throws IOException {
+    nextflowFromGLSTutorial_inner();
+  }
+
+  @Test
+  @DisplayName("nextflow config and run with GLS tutorial")
+  void nextflowFromGLSTutorial16() throws IOException {
+    nextflowFromGLSTutorial_inner();
+  }
+
+  @Test
+  @DisplayName("nextflow config and run with GLS tutorial")
+  void nextflowFromGLSTutorial17() throws IOException {
+    nextflowFromGLSTutorial_inner();
+  }
+
+  @Test
+  @DisplayName("nextflow config and run with GLS tutorial")
+  void nextflowFromGLSTutorial18() throws IOException {
+    nextflowFromGLSTutorial_inner();
+  }
+
+  @Test
+  @DisplayName("nextflow config and run with GLS tutorial")
+  void nextflowFromGLSTutorial19() throws IOException {
+    nextflowFromGLSTutorial_inner();
+  }
+
+  @Test
+  @DisplayName("nextflow config and run with GLS tutorial")
+  void nextflowFromGLSTutorial20() throws IOException {
+    nextflowFromGLSTutorial_inner();
+  }
+
+  @Test
+  @DisplayName("nextflow config and run with GLS tutorial")
+  void nextflowFromGLSTutorial21() throws IOException {
+    nextflowFromGLSTutorial_inner();
+  }
+
+  @Test
+  @DisplayName("nextflow config and run with GLS tutorial")
+  void nextflowFromGLSTutorial22() throws IOException {
+    nextflowFromGLSTutorial_inner();
+  }
+
+  @Test
+  @DisplayName("nextflow config and run with GLS tutorial")
+  void nextflowFromGLSTutorial23() throws IOException {
+    nextflowFromGLSTutorial_inner();
+  }
+
+  @Test
+  @DisplayName("nextflow config and run with GLS tutorial")
+  void nextflowFromGLSTutorial24() throws IOException {
+    nextflowFromGLSTutorial_inner();
+  }
+
+  @Test
+  @DisplayName("nextflow config and run with GLS tutorial")
+  void nextflowFromGLSTutorial25() throws IOException {
+    nextflowFromGLSTutorial_inner();
+  }
+
+  @Test
+  @DisplayName("nextflow config and run with GLS tutorial")
+  void nextflowFromGLSTutorial26() throws IOException {
+    nextflowFromGLSTutorial_inner();
+  }
+
+  @Test
+  @DisplayName("nextflow config and run with GLS tutorial")
+  void nextflowFromGLSTutorial27() throws IOException {
+    nextflowFromGLSTutorial_inner();
+  }
+
+  @Test
+  @DisplayName("nextflow config and run with GLS tutorial")
+  void nextflowFromGLSTutorial28() throws IOException {
+    nextflowFromGLSTutorial_inner();
+  }
+
+  @Test
+  @DisplayName("nextflow config and run with GLS tutorial")
+  void nextflowFromGLSTutorial29() throws IOException {
+    nextflowFromGLSTutorial_inner();
+  }
+
+  @Test
+  @DisplayName("nextflow config and run with GLS tutorial")
+  void nextflowFromGLSTutorial30() throws IOException {
+    nextflowFromGLSTutorial_inner();
+  }
+
+  @Test
+  @DisplayName("nextflow config and run with GLS tutorial")
+  void nextflowFromGLSTutorial31() throws IOException {
+    nextflowFromGLSTutorial_inner();
+  }
+
+  @Test
+  @DisplayName("nextflow config and run with GLS tutorial")
+  void nextflowFromGLSTutorial32() throws IOException {
+    nextflowFromGLSTutorial_inner();
+  }
+
+  @Test
+  @DisplayName("nextflow config and run with GLS tutorial")
+  void nextflowFromGLSTutorial33() throws IOException {
+    nextflowFromGLSTutorial_inner();
+  }
+
+  @Test
+  @DisplayName("nextflow config and run with GLS tutorial")
+  void nextflowFromGLSTutorial34() throws IOException {
+    nextflowFromGLSTutorial_inner();
+  }
+
+  @Test
+  @DisplayName("nextflow config and run with GLS tutorial")
+  void nextflowFromGLSTutorial35() throws IOException {
+    nextflowFromGLSTutorial_inner();
+  }
+
+  @Test
+  @DisplayName("nextflow config and run with GLS tutorial")
+  void nextflowFromGLSTutorial36() throws IOException {
+    nextflowFromGLSTutorial_inner();
+  }
+
+  @Test
+  @DisplayName("nextflow config and run with GLS tutorial")
+  void nextflowFromGLSTutorial37() throws IOException {
+    nextflowFromGLSTutorial_inner();
+  }
+
+  @Test
+  @DisplayName("nextflow config and run with GLS tutorial")
+  void nextflowFromGLSTutorial38() throws IOException {
+    nextflowFromGLSTutorial_inner();
+  }
+
+  @Test
+  @DisplayName("nextflow config and run with GLS tutorial")
+  void nextflowFromGLSTutorial39() throws IOException {
+    nextflowFromGLSTutorial_inner();
+  }
+
+  @Test
+  @DisplayName("nextflow config and run with GLS tutorial")
+  void nextflowFromGLSTutorial40() throws IOException {
+    nextflowFromGLSTutorial_inner();
+  }
+
+  @Test
+  @DisplayName("nextflow config and run with GLS tutorial")
+  void nextflowFromGLSTutorial41() throws IOException {
+    nextflowFromGLSTutorial_inner();
+  }
+
+  @Test
+  @DisplayName("nextflow config and run with GLS tutorial")
+  void nextflowFromGLSTutorial42() throws IOException {
+    nextflowFromGLSTutorial_inner();
+  }
+
+  @Test
+  @DisplayName("nextflow config and run with GLS tutorial")
+  void nextflowFromGLSTutorial43() throws IOException {
+    nextflowFromGLSTutorial_inner();
+  }
+
+  @Test
+  @DisplayName("nextflow config and run with GLS tutorial")
+  void nextflowFromGLSTutorial44() throws IOException {
+    nextflowFromGLSTutorial_inner();
+  }
+
+  @Test
+  @DisplayName("nextflow config and run with GLS tutorial")
+  void nextflowFromGLSTutorial45() throws IOException {
+    nextflowFromGLSTutorial_inner();
+  }
+
+  @Test
+  @DisplayName("nextflow config and run with GLS tutorial")
+  void nextflowFromGLSTutorial46() throws IOException {
+    nextflowFromGLSTutorial_inner();
+  }
+
+  @Test
+  @DisplayName("nextflow config and run with GLS tutorial")
+  void nextflowFromGLSTutorial47() throws IOException {
+    nextflowFromGLSTutorial_inner();
+  }
+
+  @Test
+  @DisplayName("nextflow config and run with GLS tutorial")
+  void nextflowFromGLSTutorial48() throws IOException {
+    nextflowFromGLSTutorial_inner();
+  }
+
+  @Test
+  @DisplayName("nextflow config and run with GLS tutorial")
+  void nextflowFromGLSTutorial49() throws IOException {
+    nextflowFromGLSTutorial_inner();
+  }
+
+  @Test
+  @DisplayName("nextflow config and run with GLS tutorial")
+  void nextflowFromGLSTutorial50() throws IOException {
     nextflowFromGLSTutorial_inner();
   }
 
